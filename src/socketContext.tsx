6c3685b9a22495907ca44320058c372448c6b04f
Victory oki <33382325+victory-oki@users.noreply.github.com>
2023-01-29 15:43:45 +0000

    log process.env

--- src/socketContext.tsx
+++ src/socketContext.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode, useContext} from 'react';
 import { io, Socket } from "socket.io-client";
 const port = 3001;
 const url= process.env.NODE_ENV==='test'?'':`http://${process.env.REACT_APP_HOST_NAME ?? 'localhost'}:${port}`;
-console.log(url);
+console.log(process.env);
 const socket: Socket = io(url);
 const SocketContext = React.createContext(socket);
 
